45211aa9f6a128e5e986395d5cce0c03935196d0
rskothapalli <raviskothapalli@gmail>
2024-04-21 14:37:13 +0530

    My 3rd file

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hello $PERSON2, how are you ?"
+echo "$PERSON2: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1: I am fine too. how is your work?"
+echo "$PERSON2: not bad. I am thinking to upgrade to devops"
