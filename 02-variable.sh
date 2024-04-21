46cbf3f3b8012bc54506fd1ca57517adf78e2579
rskothapalli <raviskothapalli@gmail>
2024-04-21 14:31:11 +0530

    this is my second file

---
+++ 02-variable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh: Hello Suresh, how are you ?"
+echo "Suresh: Hi Ramesh, I am fine. How are you?"
+echo "Ramesh: I am fine too. how is your work?"
+echo "Suresh: not bad. I am thinking to upgrade to devops"
