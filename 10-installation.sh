5b65b9e544d6bf1e9c42b3e9fa94022f27354cee
rskothapalli <raviskothapalli@gmail>
2024-04-22 22:18:01 +0530

    If else conditions

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,6 +6,17 @@ then
   exit 1
 else
   echo "You are a super user"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+  echo "Git installation failed"
+else
+  echo "git installation was success
+fi
 
 
-  dnf install msql -y
+
+
