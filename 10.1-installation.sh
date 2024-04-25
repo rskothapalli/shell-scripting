9b537d160d0d1f68d4d5ae5d1a6ba29175110917
rskothapalli <raviskothapalli@gmail>
2024-04-25 05:51:09 +0530

    commiting 10.1 with set -e

---
+++ 10.1-installation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+USERID=(id -u)
+
+if [ $USERID -ne 0 ]
+then
+  echo "Please run this script as a root"
+  set -e
+else 
+  echo "You are a super user, proceed for installation"
+fi
+ 
+ dnf install mysql -y
+
+ if [ $? -ne 0 ]
+ then
+    echo "Installation failed"
+  set -e
+  else 
+    echo "Installation is success"
+  fi
+
+  dnf install git -y
+
+  if [ $? -ne 0 ]
+  then
+    echo "Installation fo git is failed"
+  else
+    echo "Installation of git is success"
+  fi
+
+  
+
+
