6e2ae2e0312f86a5dcf0c33bb85680dfb0a192ee
rskothapalli <raviskothapalli@gmail>
2024-04-27 16:02:01 +0530

    Re-writed 10-shellscripting as 10.1

--- 10.1-installation.sh
+++ 10.1-installation.sh
@@ -1,37 +1,11 @@
 #!/bin/bash
 
-set -x
-
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [ $USERID -nq 0 ]
 then
-  echo "Please run this script as a root"
-  set -e
+  echo "Please run this script as root user"
 else 
-  echo "You are a super user, proceed for installation"
-fi
- 
- dnf install mysql -y
+  echo "You are a root user"
 
- if [ $? -ne 0 ]
- then
-    echo "Installation failed"
-  set -e
-  else 
-    echo "Installation is success"
-  fi
-
-  dnf install git -y
-
-  if [ $? -ne 0 ]
-  then
-    echo "Installation fo git is failed"
-    set -e
-  else
-    echo "Installation of git is success"
-  fi
-
-
-
-
+dnf install mysql -y
