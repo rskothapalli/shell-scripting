a4aeae1529b4a19c8da95335fc82e2237cc21f59
rskothapalli <raviskothapalli@gmail>
2024-04-27 20:54:31 +0530

    sencing

--- 12-revision.sh
+++ 12-revision.sh
@@ -1,21 +1,2 @@
-#!/bin/bash/
-
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
-then
-  echo "PLease run this script with root privilages"
-  exit 1
-else
-  echo "You are a super user"
-fi
-
-dnf install mysql -y
-
-if [ $? -ne 0 ]
-then
-  echo "mysql installatin is failed"
-else
-  echo "mysql installation is success"
-fi
-
+#!/bin/bash
+echo sume of $1 plus $2  and $3 is : "
