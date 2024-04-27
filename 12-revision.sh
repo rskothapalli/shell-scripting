28c2d5d7855eead2d616128e3e70c858c3175c5b
rskothapalli <raviskothapalli@gmail>
2024-04-27 20:16:31 +0530

    sencing

---
+++ 12-revision.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+  echo "PLease run this script with root privilages"
+else
+  echo "You are a super user"
+fi
+
+dnf install mysql -y
+
