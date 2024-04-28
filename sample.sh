484de9fcad187a038fe905111f9830132e3332b3
rskothapalli <raviskothapalli@gmail>
2024-04-28 17:32:20 +0530

    colors

--- sample.sh
+++ sample.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
-
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
-echo "$SCRIPT_NAME"
+echo "Script started executing at: $TIMESTAMP"
+
+VALIDATE(){
+  if [ $1 -ne 0 ]
+  then
+    echo -e "$2 ..$R FAILURE $N"
+    exit 1
+  else  
+    echo -e "$2 ..$G SUCCESS $N"
+  fi
+}
+
+if [ $USERID -NE 0 ]
+then
+  echo "Please run this script with root access"
+  exit 1
+else
+  echo "You are a super user"
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y &>>LOGFILE
+VALIDATE $? "Instaling Git"
+
+dnf install dockerr -y &>>LOGFILE
+VALIDATE $? "Installng Docker"
 
