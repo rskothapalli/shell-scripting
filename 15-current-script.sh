76f9ea019c4d35207343efa3604b6805707f23dc
rskothapalli <raviskothapalli@gmail.com>
2024-05-02 17:44:56 +0530

    adding two files

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -3,9 +3,9 @@ COURSE="Devops from current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./16-other-script.sh
+#./16-other-script.sh
 
-#source ./16-other-script.sh
+source ./16-other-script.sh
 
 echo "After calling other script, course: $COURSE"
 
