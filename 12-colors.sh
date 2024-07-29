b758f474f5291a96243144146bf4d2ca9abbb8bb
rskothapalli <95122252+rskothapalli@users.noreply.github.com>
2024-07-29 16:55:32 +0530

    Update 12-colors.sh

--- 12-colors.sh
+++ 12-colors.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/temp/$SCRIPT_NAME-TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
